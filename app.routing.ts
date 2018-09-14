18f2701aedc693d7094d160b8a9223d00943c94b
Franweb79 <franprietogutweb@gmail.com>
2018-09-14 17:06:55 +0200

    added menu and more styles

---
+++ app.routing.ts
@@ -0,0 +1,32 @@
+import {Routes, RouterModule} from '@angular/router';
+
+import {ModuleWithProviders} from '@angular/core';
+import {HomeComponent} from './src/app/components/home/home.component';
+import {NotfoundComponent} from './src/app/components/notfound/notfound.component';
+import {SearchComponent} from './src/app/components/search/search.component';
+
+const routes:Routes = [
+  {
+    path:'',
+    redirectTo:'home',
+    pathMatch:'full'
+  },
+  {
+    path:'home',
+    component:HomeComponent,
+    pathMatch:'full'
+  },
+  {
+    path:'search',
+    component:SearchComponent,
+    pathMatch:'full'
+  },
+  {
+    path:'**',
+    redirectTo:'home',
+    pathMatch:'full'
+  }
+
+  ];
+
+export const routing:ModuleWithProviders=RouterModule.forRoot(routes/*,{useHash:true}*/);
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,33 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import {RouterModule} from '@angular/router';
 import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
 import {MatInputModule} from '@angular/material/input';
 import {MatFormFieldModule} from '@angular/material/form-field';
 
+import {routing} from '../../app.routing';
+
 import { AppComponent } from './app.component';
+import { HomeComponent } from './components/home/home.component';
+import { NotfoundComponent } from './components/notfound/notfound.component';
+import { SearchComponent } from './components/search/search.component';
+import { ArtistComponent } from './components/artist/artist.component';
+import { NavbarComponent } from './components/shared/navbar/navbar.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    NotfoundComponent,
+    SearchComponent,
+    ArtistComponent,
+    NavbarComponent
   ],
   imports: [
     BrowserModule,
+    routing,
     BrowserAnimationsModule,
+    RouterModule,
     MatInputModule,
     MatFormFieldModule
   ],
