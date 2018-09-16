33e688e37225fefd355bfe4914c4362db6e88b82
Franweb79 <franprietogutweb@gmail.com>
2018-09-16 20:44:54 +0200

    added pipe and more optimizations

---
+++ src/app/pipes/image.pipe.ts
@@ -0,0 +1,27 @@
+import { Pipe, PipeTransform } from '@angular/core';
+
+@Pipe({
+  name: 'image'
+})
+export class ImagePipe implements PipeTransform {
+
+  transform(arrayImagenes: any[]): string {
+
+    if(!arrayImagenes)/*si es null, undefined...*/
+    {
+      /*path is so, because we have to think that app will run on index.html, so path is
+      taking that into account
+       */
+      return 'assets/img/no_image_available.jpg';
+    }
+
+    if(arrayImagenes.length>0)
+    {
+      return arrayImagenes[0].url;
+    }else{ /*si viene arreglo, pero viene sin posiciones, o vacio -que los hay-, etc*/
+      return 'assets/img/no_image_available.jpg';
+    }
+
+  }
+
+}
